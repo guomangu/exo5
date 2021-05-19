aa6ad26a7801ec4786fb637fadf3f5f2fb902979
gyomang <gyomang@gmail.com>
2021-05-19 12:13:38 +0200

    supsuprise

---
+++ taf eclipse java/testadelosmuerte/src/testadelosmuerte/wllah.java
@@ -0,0 +1,12 @@
+package testadelosmuerte;
+
+public class wllah {
+
+	public static void main(String[] args) {
+		String toi= "PIIIIRAAATE";
+		if (toi=="tu ne part pas d'ici") {
+			System.out.println("Il va falloir vous refroidir^^");
+		}
+	}
+
+}
